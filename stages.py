3236e91d00c73990da546f48e2ae6012b462efbc
ABDURAHIMAN K <laluvga@gmail.com>
2023-03-22 17:21:45 +0530

    Stage 3/5

--- stages.py
+++ stages.py
@@ -1,11 +1,49 @@
-# Stage 2/5: How do I use it?
-formats = ["plain", "bold", "italic", "header", "link", "inline-code", "ordered-list", "unordered-list", "new-line"]
-while True:
-    mode = input("Choose a formatter: ")
-    if mode not in (formats + ["!help", "!done"]):
-        print("Unknown formatting type or command")
-    elif mode == "!help":
-        print("""Available formatters: plain bold italic header link inline-code ordered-list unordered-list new-line
+# Stage 3/5: Text formatting
+class MarkDownEditor:
+    def __init__(self):
+        self.formats = ["plain", "bold", "italic", "header", "link", "inline-code", "new-line"]
+        self.text = ""
+
+    def header(self):
+        while True:
+            level = int(input("Level: "))
+            if 1 <= level <= 6:
+                break
+            print("The level should be within the range of 1 to 6")
+        self.text += "#" * level + " " + input("Text: ") + "\n"
+
+    def bold(self):
+        self.text += "**" + input("Text: ") + "**"
+
+    def italic(self):
+        self.text += "*" + input("Text: ") + "*"
+
+    def plain(self):
+        self.text += input("Text: ")
+
+    def inline_code(self):
+        self.text += "`" + input("Text: ") + "`"
+
+    def new_line(self):
+        self.text += "\n"
+
+    def link(self):
+        self.text += f"[{input('Label: ')}]({input('URL: ')})"
+
+    def start(self):
+        while True:
+            mode = input("Choose a formatter: ")
+            if mode not in (self.formats + ["!help", "!done"]):
+                print("Unknown formatting type or command")
+            elif mode == "!help":
+                print("""Available formatters: plain bold italic header link inline-code new-line
 Special commands: !help !done""")
-    elif mode == "!done":
-        break
+            elif mode == "!done":
+                break
+            else:
+                eval("self." + mode.replace("-", "_") + "()")
+                print(self.text)
+
+
+mark_down = MarkDownEditor()
+mark_down.start()
